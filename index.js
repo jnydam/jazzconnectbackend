654f303fda0edc1a6fd644cfbdaba6554bf83365
johnBrand12 <77457932+johnBrand12@users.noreply.github.com>
2021-12-02 11:30:32 -0500

    Added more logic to handle socketio

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const app = express();
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const port = 3000;
+const cors = require('cors');
 
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:19006',
+    methods: ["GET", "POST"]
+  }
+});
+
 
 
 app.use(express.json());
+app.use(cors());
 app.use(require("./routes/eventWriteRoutes"));
 app.use(require("./routes/eventReadRoutes"));
 app.use(require("./routes/venueReadRoutes"));
@@ -20,14 +28,32 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+
+
+let messages = [];
+
 io.on('connection', (socket) => {
 
   console.log("A user has connected");
 
-  socket.emit("greeting", "something");
+  socket.emit("greeting", "Welcome to JazzConnect messages!");
+
+  socket.on('chatRoomMessage', (object) => {
+
+    console.log("Here is the server object");
+    console.log(object);
+
+    messages.push(object);
+
+    socket.broadcast.emit('updateMessages', messages);
+    socket.emit('updateMessages', messages);
+  
+  });
 
 
 });
+
+
 
 
 server.listen(port, () => {
